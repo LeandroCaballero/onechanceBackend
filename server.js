43f3ea11d341114e1fb661fd6b2bc5531640a9b5
LeandroCaballero <leandrorojo248@hotmail.com>
2020-12-06 11:23:54 -0300

    Se agrega cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const path = require('path') //une las rutas de windows o linux
 const fileupload = require('express-fileupload')
 var express = require('express');
+var cors = require('cors')
 var app = express();
 require('dotenv').config()
 
 app.use(fileupload())
+app.use(cors())
 const port = process.env.PORT
 const upload = require('./routes/upload')
 
