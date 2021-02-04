f88ae1eeee23eca8ca794c583ccde269fb0c3c84
Leandro Caballero <leandrorojo248@hotmail.com>
2021-02-04 18:36:28 -0300

    Se agrega la ruta para subir un pdf a S3

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ app.use(express.json()) //el servidor entiende archivos JSON
 const port = process.env.PORT
 const upload = require('./routes/upload')
 const download = require('./routes/download')
+const uploadPDF = require('./routes/uploadPDF')
 
 app.use('/upload', upload)
 app.use('/download', download)
+app.use('/pdf', uploadPDF)
 
 app.listen(port, () => {
   console.log(`Run on port: ${port}`)
